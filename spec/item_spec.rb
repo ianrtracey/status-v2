d1f7568b438c54425c893cab5701091c39905de2
Ian Tracey <iantracey@Ians-MacBook-Pro.local>
2015-09-05 22:54:29 -0700

    Adds and Installs Devise user management with User model

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,17 +1,25 @@
 require "rails_helper"
 
 RSpec.describe Item, :type => :model do
-	it "checks to see if Team can be created" do
-		post = Post.create(name: "cs")
-		post.items.create()
+	it "checks to see if Item can be created" do
+		post = Post.create()
+		expect(post.items.create(:content => "test1", :category => "ACTIVE").valid?).to be true
+		expect(post.items.create(:content => "test2", :category => "BOOLEAN").valid?).to be true
+		expect(post.items.create(:content => "test1", :category => "COMPLETED").valid?).to be true
 	end
 
-	it "validates team names for uniqueness" do
-		expect(Team.create(name: "Testing Team").valid?).to be true
-		expect(Team.create(name: "Testing Team").valid?).to be false
+	it "checks to see if Item cannot be created witout Post" do
+		expect(Item.create(:content => "testing", :category=>"ACTIVE").valid?).to be false
 	end
 
-	it "vaidates team for name being required" do
-		expect(Team.create().valid?).to be false
+	it "verifies that the category cannot be one of the defaults" do
+		post = Post.create()
+		expect(post.items.create(:content => "testing", :category=>"FOO").valid?).to be false
 	end
+
+	# Need to test for null content
+	# Need to test for duplicate content in one post
+	# 
+
+
 end
